07d4cc369a71b9affa353e3bf6cd60d525c70477
Harshu29-09 <131910274+Harshu29-09@users.noreply.github.com>
2023-11-02 22:50:43 +0530

    day1

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+  return (
+    <div className='header'>
+    <ul>
+          <Link className='icon' to="/hello" >Day1</Link>
+         {/*  <Link className='icon' to="/About " >Day2</Link>
+           <Link  className='icon' to="/Navigation" >Day3</Link> */}
+          
+          
+    </ul>
+        
+    </div>
+  )
+}
+
+export default NavBar
