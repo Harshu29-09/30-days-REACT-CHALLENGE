f689845b991cb5a629e25d16252e4fbdbae8384e
Harshu29-09 <131910274+Harshu29-09@users.noreply.github.com>
2023-11-02 23:25:39 +0530

    day4

---
+++ src/Pages/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className="foot">
+        <footer>
+    <div className="footer">
+      <h3>Designed and Developed by Harsh Patwa</h3>
+      <span>                     Copyright © 2023 HP</span>
+    </div>
+  </footer>
+  </div>
+    
+  )
+}
+
+export default Footer
--- src/Pages/Navigation.js
+++ src/Pages/Navigation.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import './Navigation.css'
-// import Footer from './Footer'
+import Footer from './Footer'
 
 
 function Navigation() {
@@ -19,17 +19,15 @@ function Navigation() {
           <ul className="nav-links">
             <li className="hover-underline-animation"><a href="#main">Home <i className='bx bx-home-circle bx-flip-horizontal'></i></a></li>
             <li className="hover-underline-animation"><a href="#about">About <i className='bx bxl-graphql'></i></a></li>
-            {/* <li className="hover-underline-animation"><a href="#skill">Skill <i className='bx bx-code-alt'></i></a></li>
-            <li className="hover-underline-animation"><a href="#service">Service <i className='bx bxs-briefcase'></i></a></li>
-            <li className="hover-underline-animation"><a href="#project">Project <i className='bx bx-slideshow'></i></a></li> */}
+            
             <li className="hover-underline-animation"><a href="#contact">Contact <i className='bx bx-mobile-alt'></i></a></li>
             <li className="hover-underline-animation"><a href="#project">Project <i className='bx bx-slideshow'></i></a></li>
           </ul>
         </nav>
         <h1>TASK 3</h1>
         <h5>Header Build a header component and a footer component, and render them in an app.<br></br></h5>
-        {/* <Footer/>
-   */}
+        <Footer/>
+  
             
        
 
