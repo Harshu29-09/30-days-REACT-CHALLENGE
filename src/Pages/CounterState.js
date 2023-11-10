8f20e0e635ed5e85326ade1f2816dada64c217e2
Harshu29-09 <131910274+Harshu29-09@users.noreply.github.com>
2023-11-10 13:15:25 +0530

    day7

--- src/Pages/CounterState.js
+++ src/Pages/CounterState.js
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import "./CounterState.css"
-function Counter() {
+function CounterState() {
   const [count, setCount] = useState(0);
   return (
 <>
-  <h2>Task day 6</h2>
+  <h2>Task day 7</h2>
   <h4>Count: {count}</h4>
       <button className="Button"   onClick={() => setCount((c) => c + 1)}>+</button>
       <button className="Button"  onClick={() => setCount((c) => c - 1)}>-</button>
